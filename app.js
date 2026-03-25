2752d0f38d17dcfd0673502a976fa2952d08cb1b
Mauri784 <mauri784vega@gmail.com>
2026-03-24 19:41:31 -0600

    first commit

--- app.js
+++ app.js
@@ -2,12 +2,50 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var client = require('prom-client');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var itemsRouter = require('./routes/items');
 
 var app = express();
+
+// --- Prometheus ---
+client.collectDefaultMetrics();
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP procesadas',
+  labelNames: ['metodo', 'ruta', 'estado_http'],
+});
+
+const activeUsersGauge = new client.Gauge({
+  name: 'active_users_current',
+  help: 'Número actual de usuarios activos simulados',
+});
+
+// Simula usuarios activos (cambia cada 15s)
+setInterval(() => {
+  activeUsersGauge.set(Math.floor(Math.random() * 50));
+}, 15000);
+
+// Middleware que cuenta cada petición
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({
+      metodo: req.method,
+      ruta: req.path,
+      estado_http: String(res.statusCode),
+    });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+// --- Fin Prometheus ---
 
 app.use(logger('dev'));
 app.use(express.json());
